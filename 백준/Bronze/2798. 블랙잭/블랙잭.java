e51fc40fb2b1908b8f807d7c32706c9fda537968
terius-yun <tingtangting9398@gmail.com>
2024-01-05 21:59:13 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 12968 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int cardCount = in.nextInt();
+        int max = in.nextInt();
+
+        int[] cards = new int[cardCount];
+
+        for (int i = 0; i < cardCount; i++) {
+            cards[i] = in.nextInt();
+        }
+
+        int result = search(cards, cardCount, max);
+        System.out.println(result);
+    }
+
+
+    // 탐색
+    static int search(int[] cards, int cardCount, int max) {
+        int result = 0;
+
+        for (int firstCardIndex = 0; firstCardIndex < cardCount - 2; firstCardIndex++) {
+            for (int secondCardIndex = firstCardIndex + 1; secondCardIndex < cardCount - 1; secondCardIndex++) {
+                for (int thirdCardIndex = secondCardIndex + 1; thirdCardIndex < cardCount; thirdCardIndex++) {
+                    
+                    int sum = cards[firstCardIndex] + cards[secondCardIndex] + cards[thirdCardIndex];
+
+                    if (max == sum) {
+                        return sum;
+                    }
+
+                    if(result < sum && sum < max) {
+                        result = sum;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
